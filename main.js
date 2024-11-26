46313e85ea536169db6a33d12605fdf72202690a
RG <gilfanoff22@mail.ru>
2024-11-27 00:44:47 +0500

    изменения

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        icon: path.join(__dirname, 'img/icon.ico'),
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
